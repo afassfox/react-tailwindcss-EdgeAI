a56d19798611fa27a4d06019a705c78c144c7cf0
afassfox <afassfox@gmail.com>
2025-05-08 19:06:35 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { AboutUs } from "./components/sections/AboutUs";
+import { Brands } from "./components/sections/Brands";
+import { CTA } from "./components/sections/CallToAction";
+import { Hero } from "./components/sections/Hero";
+import { Pricing } from "./components/sections/Pricing";
+import { Services } from "./components/sections/Services";
+import { Layout } from "./components/shared/layout";
+
+function App() {
+  return (
+    <Layout title="EdgeAI">
+      <Hero />
+      <Brands />
+      <Services />
+      <AboutUs />
+      <Pricing />
+      <CTA />
+    </Layout>
+  );
+}
+
+export default App;
